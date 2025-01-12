c0be1176b62dfdf77d8adcbdfd4c0bc2e996d83e
Cristian Sena <crisianjsena@gmail.com>
2025-01-11 21:51:27 -0300

    Agrego boton eliminar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,8 +1,8 @@
 
-import { Dispatch } from 'react';
+import { Dispatch, useMemo } from 'react';
 import { Activity } from '../types';
 import { categories } from '../data/categories';
-import { PencilSquareIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline';
 import { ActivityActions } from '../reducers/activity-reducer';
 
 
@@ -16,12 +16,15 @@ export default function ActivityList( { activities, dispatch }: ActiviListProps 
     const categoryName = (category: Activity['category']) => 
         categories.map(cat => (cat.id === category ? cat.name : ''));
 
+    const isEmptyActivities = useMemo(() => activities.length === 0, [activities]);
+
     return (
         <>
             <h2 className='text-4xl font-bold text-slate-600 text-center'>Comida y Actividades</h2>
 
-            
-                { activities.map( activity => (
+            { isEmptyActivities 
+                ? <p className='text-center my-5'>No hay actividades aún...</p> 
+                : activities.map( activity => (
                     <div key={activity.id} className='px-5 py-10 bg-white mt-5 flex justify-between items-center rounded-lg shadow'>
                         <div className='space-y-2 relative'>
                             <p className={`absolute -top-8 -let-8 px-10 py-2 text-white uppercase font-bold 
@@ -44,11 +47,17 @@ export default function ActivityList( { activities, dispatch }: ActiviListProps 
                                         className='h-8 w-8 text-gray-800'
                                      />
                                 </button>
+                                <button
+                                    onClick={() => dispatch({ type: 'delete-activity', payload: { id: activity.id } })}
+                                >
+                                    <XCircleIcon
+                                        className='h-8 w-8 text-red-500'
+                                     />
+                                </button>
                         </div>
                     </div>
-                )) }
-            
-
+                ))
+            }
         </>
     );
 };
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,10 +1,11 @@
-import { act } from 'react';
+
 import { Activity } from '../types';
 
 
 export type ActivityActions = 
     { type: 'save-activity', payload: { newActivity: Activity } } |
-    { type: 'set-activeId', payload: { id: Activity['id'] } }
+    { type: 'set-activeId', payload: { id: Activity['id'] } } |
+    { type: 'delete-activity', payload: { id: Activity['id'] } }
 
 export type ActivityState = {
     activities: Activity[],
@@ -42,6 +43,16 @@ export const activityReducer = (
         return {
             ...state,
             activeId: action.payload.id,
+        }
+    }
+
+    if(action.type === 'delete-activity') {
+
+        const updatedActivities = state.activities.filter( activity => activity.id !== action.payload.id);
+
+        return {
+            ...state,
+            activities: updatedActivities,
         }
     }
 
